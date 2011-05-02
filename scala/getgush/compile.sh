6eff8da4c3b34be42d23db16d22ee5c1c98b93aa
Gergely Imreh <imrehg@gmail.com>
2011-05-03 02:57:52 +0800

    scala: first compile of getgush Scala "challenge"
    Based on the info at:
    http://stackoverflow.com/questions/809138/creating-a-jar-file-from-a-scala-file
    Does not actually need this, could run the core of the code in
    the scala interpreter.

---
+++ scala/getgush/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -d bin ]; then
+  rm -rf bin
+fi
+mkdir bin
+
+if [ ! -f scala-library.jar ]; then
+  cp /usr/share/scala/lib/scala-library.jar .
+fi
+
+
+scalac -sourcepath src -d bin src/getgush/ApplyTo.scala
+
+cd bin
+jar -cfm ../getgush.jar ../MANIFEST.MF *
+cd ..
+
+java -jar getgush.jar
---
+++ scala/getgush/src/getgush/ApplyTo.scala
@@ -0,0 +1,6 @@
+package getgush {
+object ApplyTo extends Application {        
+  val k = "gWU`UAXYjT[ig\\\\eBWca"        
+  println(k map {c => (c - k.size).toChar toLower})
+}
+}
